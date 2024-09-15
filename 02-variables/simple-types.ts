762bc5ae4661227cd5fe99dfd8f16dd1e8e39c0f
Samuel <slarico94@gmail.com>
2024-09-14 21:07:33 -0500

    first commit

---
+++ 02-variables/simple-types.ts
@@ -0,0 +1,8 @@
+let found = true;
+let grade = 88.6;
+let firstName = 'Caleb';
+let lastName = 'Larico';
+
+console.log(found);
+console.log(`My grade is ${grade}`);
+console.log(`Hi ${firstName} ${lastName}`);
